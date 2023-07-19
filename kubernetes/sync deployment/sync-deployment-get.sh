a69b39847931fd9f71da62640cd44c014ef06e52
sxxpqp <971944699@qq.com>
2023-07-19 17:25:27 +0800

    add 跨ns ingress+svc

--- kubernetes/sync deployment/sync-deployment-get.sh
+++ kubernetes/sync deployment/sync-deployment-get.sh
@@ -2,9 +2,10 @@
 # 获取源k8s命名空间deployment的容器的镜像
 # 用法：./get_image.sh <namespace> <deployment>
 namespace=tmc-v2-test
-kubectl -n ${namespace} get deployment | grep -v NAME | awk '{print $1}' | while read line
+kubectl -n ${namespace} get deployment | grep -v NAME |grep turing| awk '{print $1}' | while read line
 do   
-    kubectl get deployment $line  -n ${namespace} -o yaml | grep image|grep $line| awk  '{print $NF}' | while read image
+# 去掉annotation字段
+    kubectl get deployment $line  -n ${namespace} -o yaml | grep image|grep -v "apiVersion" |grep $line| awk  '{print $NF}' | while read image
     do
       echo "$line=$image"
     done
