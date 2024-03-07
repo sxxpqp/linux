459b58c24e162f4dd0de0144b16f2fdc79ee73e9
xinxin shu <sxxpqp@gmail.com>
2024-03-07 13:22:16 +0800

    add etcd restore command

--- kubernetes/sync deployment/sync-deployment-get.sh
+++ kubernetes/sync deployment/sync-deployment-get.sh
@@ -2,10 +2,13 @@
 # 获取源k8s命名空间deployment的容器的镜像
 # 用法：./get_image.sh <namespace> <deployment>
 namespace=tmc-v2-test
+# namespace=tsl
 # namespace=tmc-v2-saas
+# shellcheck disable=SC2162
 kubectl -n ${namespace} get deployment | grep -v NAME |grep turing| awk '{print $1}' | while read line
 do   
 # 去掉annotation字段
+    # shellcheck disable=SC2086
     kubectl get deployment $line  -n ${namespace} -o yaml | grep image|grep -v "apiVersion" |grep $line| awk  '{print $NF}' | while read image
     do
       echo "$line=$image"
--- kubernetes/sync deployment/sync-update.sh
+++ kubernetes/sync deployment/sync-update.sh
@@ -46,7 +46,7 @@
 # namespace=tmc-v2-saas
 # namespace=henanzhongyiyuanfushudisanyiyuan
 # namespace=hebeibosi-beimingdingyun
-namespace=jinzhongshiyuciquxiaofangjiuyuandadui
+namespace=hebeibosi-beimingdingyun
 if [ -z "$namespace" ]
 then
   echo "namespace is null"
@@ -56,31 +56,33 @@ cat<<EOF > image.txt
 turingcloud-activiti=harbor.iot.store:8085/turing-kubesphere/turingcloud-activiti:SNAPSHOT-20
 turingcloud-aircraft=harbor.iot.store:8085/turing-kubesphere/turingcloud-aircraft:SNAPSHOT-23
 turingcloud-auth=harbor.iot.store:8085/turing-kubesphere/turingcloud-auth:SNAPSHOT-48
-turingcloud-daemon-quartz=harbor.iot.store:8085/turing-kubesphere/turingcloud-daemon-quartz:SNAPSHOT-53
-turingcloud-daily=harbor.iot.store:8085/turing-kubesphere/turingcloud-daily:SNAPSHOT-282
+turingcloud-daemon-quartz=harbor.iot.store:8085/turing-kubesphere/turingcloud-daemon-quartz:SNAPSHOT-54
+turingcloud-daily=harbor.iot.store:8085/turing-kubesphere/turingcloud-daily:SNAPSHOT-284
 turingcloud-data=harbor.iot.store:8085/turing-kubesphere/turingcloud-data:SNAPSHOT-62
-turingcloud-dataanalysis=harbor.iot.store:8085/turing-kubesphere/turingcloud-dataanalysis:SNAPSHOT-332
-turingcloud-device=harbor.iot.store:8085/turing-kubesphere/turingcloud-device:SNAPSHOT-1006
+turingcloud-dataanalysis=harbor.iot.store:8085/turing-kubesphere/turingcloud-dataanalysis:SNAPSHOT-335
+turingcloud-device=harbor.iot.store:8085/turing-kubesphere/turingcloud-device:SNAPSHOT-1012
 turingcloud-gateway=harbor.iot.store:8085/turing-kubesphere/turingcloud-gateway:SNAPSHOT-23
-turingcloud-ground-pressure=harbor.iot.store:8085/turing-kubesphere/turingcloud-ground-pressure:SNAPSHOT-145
+turingcloud-ground-pressure=harbor.iot.store:8085/turing-kubesphere/turingcloud-ground-pressure:SNAPSHOT-148
 turingcloud-light=harbor.iot.store:8085/turing-kubesphere/turingcloud-light:SNAPSHOT-8
 turingcloud-register=harbor.iot.store:8085/turing-kubesphere/turingcloud-register:latest
 turingcloud-safety=harbor.iot.store:8085/turing-kubesphere/turingcloud-safety:SNAPSHOT-101
 turingcloud-tx-manager=harbor.iot.store:8085/turing-kubesphere/turingcloud-tx-manager:latest
-turingcloud-upms=harbor.iot.store:8085/turing-kubesphere/turingcloud-upms:SNAPSHOT-670
-turingcloud-video=harbor.iot.store:8085/turing-kubesphere/turingcloud-video:SNAPSHOT-445
+turingcloud-upms=harbor.iot.store:8085/turing-kubesphere/turingcloud-upms:SNAPSHOT-673
+turingcloud-video=harbor.iot.store:8085/turing-kubesphere/turingcloud-video:SNAPSHOT-451
 turingcloud-visual=harbor.iot.store:8085/turing-kubesphere/turingcloud-visual:SNAPSHOT-23
-turingcloud-web=harbor.iot.store:8085/turing-kubesphere/turingcloud-web-zktl:SNAPSHOT-1587
+turingcloud-web=harbor.iot.store:8085/turing-kubesphere/turingcloud-web-zktl:SNAPSHOT-1596
 EOF
 # 通过输入参数获取deployment.name=镜像名
+# shellcheck disable=SC2162
 while read line
 do
-  app=`echo $line | awk -F '=' '{print $1}'`
-  image=`echo $line | awk -F '=' '{print $2}'`
+  app=$(echo "$line" | awk -F '=' '{print $1}')
+  image=$(echo "$line" | awk -F '=' '{print $2}')
   echo "app=$app"
   echo "image=$image"
 #   Error: flags cannot be placed before plugin name: -n
 #   kubectl set image deployment/$app $app=$image -n ${namespace}
+    # shellcheck disable=SC2086
     kubectl set image deployment/$app $app=$image --namespace=${namespace}
 done < image.txt
 
@@ -114,3 +116,17 @@ done < image.txt
 
 # kill python process
 # ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill -9
+
+
+
+
+
+#xm vncviewer external vncviewer missing or not in the path 
+# sudo apt-get install vncviewer
+# sudo apt-get install vnc4server
+# sudo apt-get install vnc4server-core
+# sudo apt-get install vnc4server-x11
+# sudo apt-get install vnc4server-tightvnc
+# sudo apt-get install vnc4server-tigervnc
+# sudo apt-get install vnc4server-xvnc4
+# tdengine
