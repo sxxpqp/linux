8f929677c161f2e1f522fed7e9291d205b36387f
xinxin shu <sxxpqp@gmail.com>
2024-04-10 15:20:46 +0800

    add innobackup

--- kubernetes/rbac/generate-kubeconfig.md
+++ kubernetes/rbac/generate-kubeconfig.md
@@ -1,5 +1,5 @@
 ```
-kubectl create sa my-sa -n user-system
+kubectl create sa my-sa -n tmc-v2-test
 ```
 ```
 cat>role-sa.yaml<<EOF
@@ -11,7 +11,7 @@ metadata:
   labels:
     kubernetes.io/bootstrapping: rbac-defaults
   name: myrole
-  namespace: user-system
+  namespace: tmc-v2-test
 rules:
 - apiGroups:
   - "apps"
@@ -23,7 +23,7 @@ rules:
   - watch
 EOF
 
-kubectl create -f role-sa.yaml -n user-system
+kubectl create -f role-sa.yaml -n tmc-v2-test
 
 ```
 ```
@@ -32,7 +32,7 @@ apiVersion: rbac.authorization.k8s.io/v1
 kind: RoleBinding
 metadata:
   name: myrolebinding
-  namespace: user-system
+  namespace: tmc-v2-test
 roleRef:
   apiGroup: rbac.authorization.k8s.io
   kind: Role
@@ -40,29 +40,29 @@ roleRef:
 subjects:
 - kind: ServiceAccount
   name: my-sa
-  namespace: user-system
+  namespace: tmc-v2-test
 EOF
 
-kubectl create -f myrolebinding.yaml -n user-system
+kubectl create -f myrolebinding.yaml -n tmc-v2-test
 
 ```
 ```
-kubectl get secret -n user-system |grep my-sa
-kubectl get secret my-sa-token-k6kxt -n user-system -oyaml |grep ca.crt: | awk '{print $2}' |base64 -d > /home/ca.crt
+kubectl get secret -n tmc-v2-test |grep my-sa
+kubectl get secret my-sa-token-rl2df -n tmc-v2-test -oyaml |grep ca.crt: | awk '{print $2}' |base64 -d > /home/ca.crt
 
 ```
 设置集群访问方式，其中test-arm为需要访问的集群，10.0.1.100为集群apiserver地址（获取方法参见图1），/home/test.config为配置文件的存放路径。
 如果通过内部apiserver地址，执行如下命令：
 ```
-kubectl config set-cluster test-arm --server=https://lb.kubesphere.local:6443  --certificate-authority=/home/ca.crt  --embed-certs=true --kubeconfig=/home/test.config
+kubectl config set-cluster test-arm --server=https://139.198.122.166:6443  --certificate-authority=/home/ca.crt  --embed-certs=true --kubeconfig=/home/test.config
 ```
 如果通过公网apiserver地址，执行如下命令：
 ```
-kubectl config set-cluster test-arm --server=https://10.0.1.100:5443 --kubeconfig=/home/test.config --insecure-skip-tls-verify=true
+kubectl config set-cluster test-arm --server=https://139.198.122.166:6443 --kubeconfig=/home/test.config --insecure-skip-tls-verify=true
 ```
 
 ```
-token=$(kubectl describe secret my-sa-token-k6kxt -n user-system | awk '/token:/{print $2}')
+token=$(kubectl describe secret my-sa-token-rl2df -n tmc-v2-test | awk '/token:/{print $2}')
 
 ```
 ```
--- kubernetes/sync deployment/sync-update.sh
+++ kubernetes/sync deployment/sync-update.sh
@@ -46,7 +46,9 @@
 # namespace=tmc-v2-saas
 # namespace=henanzhongyiyuanfushudisanyiyuan
 # namespace=hebeibosi-beimingdingyun
-namespace=hebeibosi-beimingdingyun
+# namespace=kean-xiaofang
+# namespace=tmc-v2-saas
+namespace=tmc-sd
 if [ -z "$namespace" ]
 then
   echo "namespace is null"
@@ -54,23 +56,22 @@ then
 fi
 cat<<EOF > image.txt
 turingcloud-activiti=harbor.iot.store:8085/turing-kubesphere/turingcloud-activiti:SNAPSHOT-20
-turingcloud-aircraft=harbor.iot.store:8085/turing-kubesphere/turingcloud-aircraft:SNAPSHOT-23
-turingcloud-auth=harbor.iot.store:8085/turing-kubesphere/turingcloud-auth:SNAPSHOT-48
+turingcloud-auth=harbor.iot.store:8085/turing-kubesphere/turingcloud-auth:SNAPSHOT-50
 turingcloud-daemon-quartz=harbor.iot.store:8085/turing-kubesphere/turingcloud-daemon-quartz:SNAPSHOT-54
-turingcloud-daily=harbor.iot.store:8085/turing-kubesphere/turingcloud-daily:SNAPSHOT-284
-turingcloud-data=harbor.iot.store:8085/turing-kubesphere/turingcloud-data:SNAPSHOT-62
-turingcloud-dataanalysis=harbor.iot.store:8085/turing-kubesphere/turingcloud-dataanalysis:SNAPSHOT-335
-turingcloud-device=harbor.iot.store:8085/turing-kubesphere/turingcloud-device:SNAPSHOT-1012
+turingcloud-daily=harbor.iot.store:8085/turing-kubesphere/turingcloud-daily:SNAPSHOT-286
+turingcloud-dataanalysis=harbor.iot.store:8085/turing-kubesphere/turingcloud-dataanalysis:SNAPSHOT-338
+turingcloud-device=harbor.iot.store:8085/turing-kubesphere/turingcloud-device:SNAPSHOT-1026
 turingcloud-gateway=harbor.iot.store:8085/turing-kubesphere/turingcloud-gateway:SNAPSHOT-23
-turingcloud-ground-pressure=harbor.iot.store:8085/turing-kubesphere/turingcloud-ground-pressure:SNAPSHOT-148
+turingcloud-ground-pressure=harbor.iot.store:8085/turing-kubesphere/turingcloud-ground-pressure:SNAPSHOT-122
 turingcloud-light=harbor.iot.store:8085/turing-kubesphere/turingcloud-light:SNAPSHOT-8
+turingcloud-monitor=harbor.iot.store:8085/turing-kubesphere/turingcloud-monitor:SNAPSHOT-10
 turingcloud-register=harbor.iot.store:8085/turing-kubesphere/turingcloud-register:latest
-turingcloud-safety=harbor.iot.store:8085/turing-kubesphere/turingcloud-safety:SNAPSHOT-101
+turingcloud-safety=harbor.iot.store:8085/turing-kubesphere/turingcloud-safety:SNAPSHOT-102
 turingcloud-tx-manager=harbor.iot.store:8085/turing-kubesphere/turingcloud-tx-manager:latest
-turingcloud-upms=harbor.iot.store:8085/turing-kubesphere/turingcloud-upms:SNAPSHOT-673
-turingcloud-video=harbor.iot.store:8085/turing-kubesphere/turingcloud-video:SNAPSHOT-451
+turingcloud-upms=harbor.iot.store:8085/turing-kubesphere/turingcloud-upms:SNAPSHOT-690
+turingcloud-video=harbor.iot.store:8085/turing-kubesphere/turingcloud-video:SNAPSHOT-461
 turingcloud-visual=harbor.iot.store:8085/turing-kubesphere/turingcloud-visual:SNAPSHOT-23
-turingcloud-web=harbor.iot.store:8085/turing-kubesphere/turingcloud-web-zktl:SNAPSHOT-1596
+turingcloud-web=harbor.iot.store:8085/turing-kubesphere/turingcloud-web-zktl:SNAPSHOT-1625
 EOF
 # 通过输入参数获取deployment.name=镜像名
 # shellcheck disable=SC2162
@@ -84,6 +85,7 @@ do
 #   kubectl set image deployment/$app $app=$image -n ${namespace}
     # shellcheck disable=SC2086
     kubectl set image deployment/$app $app=$image --namespace=${namespace}
+    # kubectl scale --replicas=0 deployment/$app  --namespace=${namespace}
 done < image.txt
 
 
